8d46d19d263822ab5964ca3cb807a3d547a391b0
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-08-09 16:35:53 +0530

    Add files via upload

---
+++ String_str1.java
@@ -0,0 +1,46 @@
+public class String_str1 {
+    public static void main(String[] args) {
+
+    //1. Concatenate two strings "Hello" and "World".
+   /* String str1="Hello";
+    String str2="World";
+
+    String str3=str1.concat(str2);
+    System.out.println(str1.concat(str2));
+*/
+
+
+
+   //2. Find the length of the string "OpenAI"
+
+  /* String str1="OpenAI";
+   System.out.println(str1.length());
+    */
+
+
+
+    //3. Access the first character of the string "ChatGPT"
+
+    /*String str1="ChatGPT";
+    char ch = str1.charAt(0);
+    System.out.println(ch);*/
+
+
+
+    //4. Access the last character of the string "Intelligence"
+
+    /*String str = "Intelligence";
+    char lastch = str.charAt(str.length()-1);
+    System.out.println(lastch); */
+
+
+    //5.Extract the substring "GPT" from "ChatGPT"
+   
+     String str = "ChatGPT";
+    String substring = str.substring(4); 
+    System.out.println("The extracted substring is: " + substring);
+    }
+
+
+    
+}
