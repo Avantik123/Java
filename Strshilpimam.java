07c45696c4f113bc496edcc6a59cc14dbe941b9e
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-08-11 22:40:40 +0530

    Add files via upload

---
+++ Strshilpimam.java
@@ -0,0 +1,40 @@
+class strshilpimam{
+    public static void main(String[] args) {
+
+//Q1. Reverse of thye string
+
+      
+/*  
+        String str = "__Shipli jain__";
+        for(int i=str.length()-1; i>=0;i--){
+         //System.out.println(str[i]);
+         char ch =str.charAt(i);
+        }
+        System.out.println(ch);
+*/
+ //Q2. character count in the string
+
+ /*
+        String str="Hello world";
+        int count=0;
+        for(int i=0;i<str.length();i++){
+        char ch =str.charAt(i);
+        count++;
+        }
+     System.out.println(count);
+*/
+
+
+//Q3. Check the string is null or not
+
+    String str ="Hello";
+   // System.out.println(str);
+
+    if(str==null){
+        System.out.println("String is Empty");
+    }
+    else{
+        System.out.println("String is n ot Empty");
+    }
+    }
+}
