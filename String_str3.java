ce1464c0d0fa3cc248a61de56637f5ac2a0e5cde
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-08-16 11:35:20 +0530

    Add files via upload

---
+++ String_str3.java
@@ -0,0 +1,25 @@
+public class String_str3{
+    public static void main(String[] args) {
+         
+        //11.Find the index of the first occurrence of "e" in "OpenAI".
+/*
+        String str ="OpenAI";
+        int index=str.indexOf('e');
+        System.out.println("The index of the first occurrence of 'e' is:"+index);
+*/
+
+       //12.Find the index of the last occurrence of "n" in "Intelligent".
+/*
+       String str ="Intelligent";
+       int index=str.lastIndexOf('n');
+       System.out.println("The index of the last occurrence of 'n' in Intelligent is:" +index);
+
+*/
+
+      //13. Check if the string "Hello" starts with "He".
+      String str = "Hello";
+        boolean startsWithHe = str.startsWith("He");
+        System.out.println("Does the string start with 'He'? " + startsWithHe);
+    }
+
+}
